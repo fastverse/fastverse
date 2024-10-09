1db1cd86f878e6cca407636e66bef6e3bbd6200b
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-10-09 12:36:44 +0200

    Add condition.

--- R/update.R
+++ R/update.R
@@ -34,6 +34,12 @@ fastversechild_deps <- function(pkg = fastversechild_packages(), recursive = FAL
     pkg_deps <- if(include.self) c(pkg, fv) else pkg
   }
   
+  if(!all(pnmiss <- pkg_deps %in% rownames(pkgs))) {
+    warning(paste("Ignoring package(s)",  paste(pkg_deps[!pnmiss], collapse = ", "), "not available on CRAN"))
+    pkg_deps <- pkg_deps[pnmiss]
+    if(!length(pkg_deps)) return()
+  }
+  
   cran_version <- lapply(pkgs[pkg_deps, "Version"], base::package_version)
   local_version <- lapply(pkg_deps, packageVersion2)
   
